1c122ea761df975e613cc19f31e6da675ba7a903
Vladislav Savchenko <vs101ff@gmail.com>
2021-04-20 14:19:09 +0300

    refactor: extract MenuItemLink

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,8 +3,56 @@ import { Flex, chakra, Box, Link, Button } from "@chakra-ui/react";
 import LogoLink from "./LogoLink";
 import NextLink from "next/link";
 
+interface MenuItem {
+  title: string;
+  id: number;
+  href: string;
+}
+
+const MenuItemLink = (menuItem: MenuItem) => {
+  const { href, title } = menuItem;
+
+  return (
+    <NextLink href={href}>
+      <Link
+        colorScheme={"gray"}
+        px={"4"}
+        display={"block"}
+        fontSize={"lg"}
+        lineHeight={"10"}
+        textAlign={"center"}
+        p={"6"}
+        h={"full"}
+        whiteSpace={"nowrap"}
+        textDecoration={"none"}
+        transition={"color 150ms ease-in-out 0s"}
+        sx={{
+          mixBlendMode: "normal",
+        }}
+      >
+        {title}
+        <chakra.svg
+          w="10px"
+          h="6px"
+          position={"relative"}
+          display={"inline-block"}
+          left={"0.4rem"}
+          top={"-0.2rem"}
+          stroke={"currentcolor"}
+          transition={"stroke 150ms ease-in-out 0s"}
+          viewBox="0 0 10 6"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M9 1L5 5 1 1" />
+        </chakra.svg>
+      </Link>
+    </NextLink>
+  );
+};
+
 const Header = () => {
-  const menuItems = [
+  const menuItems: MenuItem[] = [
     {
       title: "Platform",
       id: 1,
@@ -31,41 +79,7 @@ const Header = () => {
         <Flex as={"ul"}>
           {menuItems.map((x) => (
             <Box as={"li"} key={x.id} flex={"0 0 auto"}>
-              <NextLink href={x.href}>
-                <Link
-                  colorScheme={"gray"}
-                  px={"4"}
-                  display={"block"}
-                  fontSize={"lg"}
-                  lineHeight={"10"}
-                  textAlign={"center"}
-                  p={"6"}
-                  h={"full"}
-                  whiteSpace={"nowrap"}
-                  textDecoration={"none"}
-                  transition={"color 150ms ease-in-out 0s"}
-                  sx={{
-                    mixBlendMode: "normal",
-                  }}
-                >
-                  {x.title}
-                  <chakra.svg
-                    w="10px"
-                    h="6px"
-                    position={"relative"}
-                    display={"inline-block"}
-                    left={"0.4rem"}
-                    top={"-0.2rem"}
-                    stroke={"currentcolor"}
-                    transition={"stroke 150ms ease-in-out 0s"}
-                    viewBox="0 0 10 6"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M9 1L5 5 1 1" />
-                  </chakra.svg>
-                </Link>
-              </NextLink>
+              <MenuItemLink {...x} />
             </Box>
           ))}
         </Flex>
