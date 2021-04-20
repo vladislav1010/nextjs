d6877721b3dc1724f634588955945bf66afbd3bc
Vladislav Savchenko <vs101ff@gmail.com>
2021-04-20 15:51:40 +0300

    feat: add example of height transition

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,13 @@
 import * as React from "react";
-import { Flex, chakra, Box, Link, Button } from "@chakra-ui/react";
+import {
+  Flex,
+  chakra,
+  Box,
+  Link,
+  Button,
+  HTMLChakraProps,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import LogoLink from "./LogoLink";
 import NextLink from "next/link";
 
@@ -9,7 +17,96 @@ interface MenuItem {
   href: string;
 }
 
-const MenuItemLink = (menuItem: MenuItem) => {
+interface DropdownProps extends HTMLChakraProps<"div"> {
+  open: boolean;
+}
+const Dropdown = ({ open, children, ...rest }: DropdownProps) => {
+  return (
+    <Box
+      visibility={open ? "visible" : "hidden"}
+      position={{ base: "relative", xl: "absolute" }}
+      transition={"max-height 300ms ease 0s"}
+      pointerEvents={open ? "auto" : "none"}
+      maxH={{ base: open ? "30rem" : 0, xl: "initial" }}
+      h={{ base: "full", xl: "initial" }}
+      overflow={"hidden"}
+      w={{ xl: "full" }}
+      bg={{ xl: useColorModeValue("white", "black") }}
+      padding={{ xl: 0 }}
+      left={{ xl: 0 }}
+      top={{ xl: "100%" }}
+      maxW={{ xl: "120rem" }}
+      {...rest}
+    >
+      <Box
+        as={"section"}
+        w={"full"}
+        opacity={open ? 1 : 0}
+        visibility={open ? "visible" : "hidden"}
+        h={open ? "38rem" : 0}
+        overflow={"hidden"}
+        transition={"opacity 250ms linear 0s, height 250ms ease-out 0s"}
+      >
+        {children}
+      </Box>
+    </Box>
+  );
+};
+
+const callAll = (...fns: (((...args: any[]) => void) | undefined)[]) => (
+  ...args: any[]
+) => fns.forEach((fn) => fn?.(...args));
+
+const useDropdownTrigger = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleMouseEnter = React.useCallback(() => {
+    setOpen(() => true);
+  }, []);
+  const handleMouseLeave = React.useCallback(() => {
+    setOpen(() => false);
+  }, []);
+
+  return {
+    getProps: function <T>({
+      onMouseEnter,
+      onMouseLeave,
+      ...rest
+    }: {
+      onMouseEnter?: React.MouseEventHandler;
+      onMouseLeave?: React.MouseEventHandler;
+    } & T) {
+      return {
+        onMouseEnter: callAll(onMouseEnter, handleMouseEnter),
+        onMouseLeave: callAll(onMouseLeave, handleMouseLeave),
+        ...rest,
+      };
+    },
+    open,
+    setOpen,
+  };
+};
+
+interface MenuItemProps extends MenuItemLinkProps {
+  dropdownElement: React.ReactElement;
+}
+const MenuItem = ({ dropdownElement, ...props }: MenuItemProps) => {
+  const { getProps, open } = useDropdownTrigger();
+
+  return (
+    <>
+      <MenuItemLink {...getProps(props)} />
+      {React.cloneElement(dropdownElement, {
+        open,
+      })}
+    </>
+  );
+};
+
+interface MenuItemLinkProps extends HTMLChakraProps<"a"> {
+  menuItem: MenuItem;
+}
+const MenuItemLink = ({ menuItem, ...rest }: MenuItemLinkProps) => {
   const { href, title } = menuItem;
 
   return (
@@ -21,7 +118,7 @@ const MenuItemLink = (menuItem: MenuItem) => {
         fontSize={"lg"}
         lineHeight={"10"}
         textAlign={"center"}
-        p={"6"}
+        p={{ xl: "6" }}
         h={"full"}
         whiteSpace={"nowrap"}
         textDecoration={"none"}
@@ -29,6 +126,7 @@ const MenuItemLink = (menuItem: MenuItem) => {
         sx={{
           mixBlendMode: "normal",
         }}
+        {...rest}
       >
         {title}
         <chakra.svg
@@ -75,11 +173,15 @@ const Header = () => {
       <Flex flex={"0 0 auto"} align={"center"}>
         <LogoLink />
       </Flex>
-      <Flex as={"nav"} flex={"0 0 auto"} align={"center"}>
+      <Flex as={"nav"} flex={"0 0 auto"} align={"center"} position={"relative"}>
         <Flex as={"ul"}>
           {menuItems.map((x) => (
-            <Box as={"li"} key={x.id} flex={"0 0 auto"}>
-              <MenuItemLink {...x} />
+            <Box as={"li"} h={{ xl: "24" }} key={x.id} flex={"0 0 auto"}>
+              <MenuItem
+                // @ts-expect-error
+                dropdownElement={<Dropdown>Some content</Dropdown>}
+                menuItem={x}
+              />
             </Box>
           ))}
         </Flex>
